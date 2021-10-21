fbf26c0d8b72bebb29db3f75b25ff83cbaf8a574
wanyi2882 <wanyi.2882@gmail.com>
2021-10-21 02:59:36 +0000

    Chinook practice updated

--- chinook-ddl.sql
+++ chinook-ddl.sql
@@ -1,14 +1,18 @@
 /* 1. Display all Sales Support Agents with their first name and last name */
 
-SELECT title, firstName, lastName FROM Employee 
-WHERE title='Sales Support Agent';
+SELECT Title, firstName, lastName FROM Employee 
+WHERE Title='Sales Support Agent';
 
 /* 2. Display all employees hired between 2002 and 2003, 
 and display their first name and last name */
 
-SELECT firstName, lastName, HireDate FROM Employee 
+SELECT FirstName, LastName, HireDate FROM Employee 
 WHERE HireDate
 BETWEEN "2002-01-01" AND "2003-01-01";
+
+SELECT FirstName, LastName, HireDate FROM Employee 
+WHERE YEAR(HireDate)
+BETWEEN 2002 AND 2003;
 
 /* 3. Display all artists that have the word 'Metal' in their name */
 
@@ -18,7 +22,7 @@ WHERE Name LIKE "%metal%";
 /* 4. Display all employees who are in sales (sales manager, sales rep etc.) */
 
 SELECT LastName, FirstName, Title FROM Employee
-WHERE title LIKE "%sales%";
+WHERE Title LIKE "%sales%";
 
 /* 5. Display the titles of all tracks which has the genre "easy listening" */
 
@@ -33,7 +37,7 @@ JOIN Album
 ON Album.AlbumId = Track.AlbumId
 JOIN Genre
 on Genre.GenreId = Track.GenreId
-LIMIT 10;
+LIMIT 10 OFFSET 5;
 
 /* 7. Using the Invoice table, show the average payment made for each country */
 SELECT BillingCountry, AVG(Total) FROM Invoice
@@ -47,11 +51,17 @@ HAVING AVG(Total)>5.5;
 
 /* 9 - Using the Invoice table, show the average payment made for each customer, 
 but only for customer reside in Germany and only if that customer has paid more than 10in total */
-SELECT Customer.CustomerID, SUM(Total), Country From Invoice
+SELECT Customer.CustomerId, SUM(Total), Country From Invoice
 JOIN Customer
-ON Customer.CustomerID = Invoice.CustomerID
-GROUP BY Customer.CustomerID
+ON Customer.CustomerId = Invoice.CustomerId
+GROUP BY Customer.CustomerId
 HAVING Country="Germany" AND SUM(Total)>10;
+
+/* Correct Answer: */
+SELECT CustomerId, BillingCountry, AVG(Total) From Invoice
+WHERE BillingCountry = "Germany"
+GROUP BY CustomerId
+HAVING SUM(Total) > 10;
 
 /* 10 - Display the average length of Jazz song (that is, the genre of the song is Jazz) for each album */
 SELECT 
@@ -63,4 +73,4 @@ FROM Track
 JOIN Album ON Track.AlbumId = Album.AlbumId
 JOIN Genre ON Track.GenreId = Genre.GenreId
 WHERE Genre.Name="Jazz"
-GROUP BY Album.AlbumId;
+GROUP BY Album.AlbumId, Album.Title, Genre.Name;
